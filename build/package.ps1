791e05f13a6bc15d8295c108573955b66226eb05
Norgerman <xyn0410@gmail.com>
2019-03-02 20:42:22 +0800

    add tool build

--- build/package.ps1
+++ build/package.ps1
@@ -6,7 +6,12 @@ dotnet publish -c Release -r win10-x64 Shark.Client
 dotnet publish -c Release -r osx.10.12-x64 Shark.Client
 dotnet publish -c Release -r ubuntu.18.04-x64 Shark.Client
 
+dotnet pack -c Release Shark.Client.Tool
+dotnet pack -c Release Shark.Server.Tool
+
 Add-Type -A System.IO.Compression.FileSystem
+
+Remove-Item * -Include *.zip,*.nupkg
 
 #win10-x64
 $inPath = Join-Path (Get-Item -Path ".\").FullName 'Shark.Server\bin\Release\netcoreapp2.2\win10-x64\publish'
@@ -35,3 +40,7 @@ $outPath = Join-Path (Get-Item -Path ".\").FullName 'shark-server-ubuntu.18.04-x
 $inPath = Join-Path (Get-Item -Path ".\").FullName 'Shark.Client\bin\Release\netcoreapp2.2\ubuntu.18.04-x64\publish'
 $outPath = Join-Path (Get-Item -Path ".\").FullName 'shark-ubuntu.18.04-x64.zip'
 [IO.Compression.ZipFile]::CreateFromDirectory($inPath, $outPath, [System.IO.Compression.CompressionLevel]::Optimal, $False)
+
+#tool packages
+Move-Item 'Shark.Client.Tool\bin\Release\*.nupkg' .\
+Move-Item 'Shark.Server.Tool\bin\Release\*.nupkg' .\
