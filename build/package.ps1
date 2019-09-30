46f253f61d846f62d005e09d32e69165ea80afe1
Norgerman <xyn0410@gmail.com>
2019-09-30 14:52:18 +0800

    update build script

--- build/package.ps1
+++ build/package.ps1
@@ -15,30 +15,30 @@ Add-Type -A System.IO.Compression.FileSystem
 Remove-Item * -Include *.zip,*.nupkg
 
 #win10-x64
-$inPath = Join-Path (Get-Item -Path ".\").FullName 'Shark.Server\bin\Release\netcoreapp2.2\win10-x64\publish'
+$inPath = Join-Path (Get-Item -Path ".\").FullName 'Shark.Server\bin\Release\netcoreapp3.0\win10-x64\publish'
 $outPath = Join-Path (Get-Item -Path ".\").FullName 'shark-server-win10-x64.zip'
 [IO.Compression.ZipFile]::CreateFromDirectory($inPath, $outPath, [System.IO.Compression.CompressionLevel]::Optimal, $False)
 
-$inPath = Join-Path (Get-Item -Path ".\").FullName 'Shark.Client\bin\Release\netcoreapp2.2\win10-x64\publish'
+$inPath = Join-Path (Get-Item -Path ".\").FullName 'Shark.Client\bin\Release\netcoreapp3.0\win10-x64\publish'
 $outPath = Join-Path (Get-Item -Path ".\").FullName 'shark-win10-x64.zip'
 [IO.Compression.ZipFile]::CreateFromDirectory($inPath, $outPath, [System.IO.Compression.CompressionLevel]::Optimal, $False)
 
 #osx.10.12-x64
-$inPath = Join-Path (Get-Item -Path ".\").FullName 'Shark.Server\bin\Release\netcoreapp2.2\osx.10.12-x64\publish'
+$inPath = Join-Path (Get-Item -Path ".\").FullName 'Shark.Server\bin\Release\netcoreapp3.0\osx.10.12-x64\publish'
 $outPath = Join-Path (Get-Item -Path ".\").FullName 'shark-server-osx.10.12-x64.zip'
 [IO.Compression.ZipFile]::CreateFromDirectory($inPath, $outPath, [System.IO.Compression.CompressionLevel]::Optimal, $False)
 
-$inPath = Join-Path (Get-Item -Path ".\").FullName 'Shark.Client\bin\Release\netcoreapp2.2\osx.10.12-x64\publish'
+$inPath = Join-Path (Get-Item -Path ".\").FullName 'Shark.Client\bin\Release\netcoreapp3.0\osx.10.12-x64\publish'
 $outPath = Join-Path (Get-Item -Path ".\").FullName 'shark-osx.10.12-x64.zip'
 [IO.Compression.ZipFile]::CreateFromDirectory($inPath, $outPath, [System.IO.Compression.CompressionLevel]::Optimal, $False)
 
 
 #ubuntu.18.04-x64
-$inPath = Join-Path (Get-Item -Path ".\").FullName 'Shark.Server\bin\Release\netcoreapp2.2\ubuntu.18.04-x64\publish'
+$inPath = Join-Path (Get-Item -Path ".\").FullName 'Shark.Server\bin\Release\netcoreapp3.0\ubuntu.18.04-x64\publish'
 $outPath = Join-Path (Get-Item -Path ".\").FullName 'shark-server-ubuntu.18.04-x64.zip'
 [IO.Compression.ZipFile]::CreateFromDirectory($inPath, $outPath, [System.IO.Compression.CompressionLevel]::Optimal, $False)
 
-$inPath = Join-Path (Get-Item -Path ".\").FullName 'Shark.Client\bin\Release\netcoreapp2.2\ubuntu.18.04-x64\publish'
+$inPath = Join-Path (Get-Item -Path ".\").FullName 'Shark.Client\bin\Release\netcoreapp3.0\ubuntu.18.04-x64\publish'
 $outPath = Join-Path (Get-Item -Path ".\").FullName 'shark-ubuntu.18.04-x64.zip'
 [IO.Compression.ZipFile]::CreateFromDirectory($inPath, $outPath, [System.IO.Compression.CompressionLevel]::Optimal, $False)
 
