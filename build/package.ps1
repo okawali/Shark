ca0ffb87e8249a56340b2ceb023087b9e6b83e74
Norgerman <xyn0410@gmail.com>
2021-11-12 11:47:37 +0800

    net 6.0

--- build/package.ps1
+++ build/package.ps1
@@ -15,29 +15,29 @@ Add-Type -A System.IO.Compression.FileSystem
 Remove-Item * -Include *.zip,*.nupkg
 
 #win10-x64
-$inPath = Join-Path (Get-Item -Path "./").FullName 'Shark.Server/bin/Release/net5.0/win10-x64/publish'
+$inPath = Join-Path (Get-Item -Path "./").FullName 'Shark.Server/bin/Release/net6.0/win10-x64/publish'
 $outPath = Join-Path (Get-Item -Path "./").FullName 'shark-server-win10-x64.zip'
 [IO.Compression.ZipFile]::CreateFromDirectory($inPath, $outPath, [System.IO.Compression.CompressionLevel]::Optimal, $False)
 
-$inPath = Join-Path (Get-Item -Path "./").FullName 'Shark.Client/bin/Release/net5.0/win10-x64/publish'
+$inPath = Join-Path (Get-Item -Path "./").FullName 'Shark.Client/bin/Release/net6.0/win10-x64/publish'
 $outPath = Join-Path (Get-Item -Path "./").FullName 'shark-win10-x64.zip'
 [IO.Compression.ZipFile]::CreateFromDirectory($inPath, $outPath, [System.IO.Compression.CompressionLevel]::Optimal, $False)
 
 #osx.10.12-x64
-$inPath = Join-Path (Get-Item -Path "./").FullName 'Shark.Server/bin/Release/net5.0/osx.10.12-x64/publish'
+$inPath = Join-Path (Get-Item -Path "./").FullName 'Shark.Server/bin/Release/net6.0/osx.10.12-x64/publish'
 $outPath = Join-Path (Get-Item -Path "./").FullName 'shark-server-osx.10.12-x64.zip'
 [IO.Compression.ZipFile]::CreateFromDirectory($inPath, $outPath, [System.IO.Compression.CompressionLevel]::Optimal, $False)
 
-$inPath = Join-Path (Get-Item -Path "./").FullName 'Shark.Client/bin/Release/net5.0/osx.10.12-x64/publish'
+$inPath = Join-Path (Get-Item -Path "./").FullName 'Shark.Client/bin/Release/net6.0/osx.10.12-x64/publish'
 $outPath = Join-Path (Get-Item -Path "./").FullName 'shark-osx.10.12-x64.zip'
 [IO.Compression.ZipFile]::CreateFromDirectory($inPath, $outPath, [System.IO.Compression.CompressionLevel]::Optimal, $False)
 
 
 #ubuntu.18.04-x64
-$inPath = Join-Path (Get-Item -Path "./").FullName 'Shark.Server/bin/Release/net5.0/linux-x64/publish'
+$inPath = Join-Path (Get-Item -Path "./").FullName 'Shark.Server/bin/Release/net6.0/linux-x64/publish'
 $outPath = Join-Path (Get-Item -Path "./").FullName 'shark-server-linux-x64.zip'
 [IO.Compression.ZipFile]::CreateFromDirectory($inPath, $outPath, [System.IO.Compression.CompressionLevel]::Optimal, $False)
 
-$inPath = Join-Path (Get-Item -Path "./").FullName 'Shark.Client/bin/Release/net5.0/linux-x64/publish'
+$inPath = Join-Path (Get-Item -Path "./").FullName 'Shark.Client/bin/Release/net6.0/linux-x64/publish'
 $outPath = Join-Path (Get-Item -Path "./").FullName 'shark-linux-x64.zip'
 [IO.Compression.ZipFile]::CreateFromDirectory($inPath, $outPath, [System.IO.Compression.CompressionLevel]::Optimal, $False)
